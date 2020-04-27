c4b2be3ff1ba688f7ce165109f607b19b2771b57
Scott Key <scottdkey@gmail.com>
2020-04-27 15:11:23 -0600

    Responsive functional

--- src/App.js
+++ src/App.js
@@ -35,31 +35,33 @@ function App() {
   return (
     <div className="App">
       <div style={styles.banner}>
-        <div style={styles.imageContainer}>
-          <img src={CovidImage} alt="COVID19" style={styles.covid} />
-        </div>
-        <div style={styles.textContainer}>
-          <h1>Corona Virus Top 10 Stats</h1>
-          <p>update at: {lastChecked}</p>
-          <p>data points: {covid19Stats.length}</p>
+        <div style={styles.bannerTextContainer}>
+          <h1 style={styles.header}>COVID-19 Cases by Country</h1>
+          <p>Data provided by Johns Hopkins University</p>
+          <div style={styles.imageContainer}>
+            <img src={CovidImage} alt="COVID19" style={styles.covid} />
+          </div>
         </div>
       </div>
-      <div style={styles.row}>
-        <div style={styles.column}>
-          <Graph covid19Stats={covid19Stats} />
+      <div style={styles.body}>
+        <div style={styles.statsContainer}>
+          <div style={styles.graphColumn}>
+            <Graph covid19Stats={covid19Stats} />
+          </div>
+          <p style={styles.apiInfoColumn}>
+            This API uses publicly available data about current confirmed cases,
+            deaths, and recoveries of the COVID-19 virus AKA Coronavirus
+            compiled by Johns Hopkins University. Accepts: country as filter
+            parameter, otherwise returns all stats. Country name must match
+            exactly with what is in the data (URL encoded spaces and
+            punctuation) For some reason RapidAPI counts 304 "Not Modified"
+            responses as "errors". This is the reason for the high "error count"
+            of this API.
+            <a href="https://rapidapi.com/KishCom/api/covid-19-coronavirus-statistics/details">
+              More information on this API can be found here.
+            </a>
+          </p>
         </div>
-        <p style={styles.column}>
-          This API uses publicly available data about current confirmed cases,
-          deaths, and recoveries of the COVID-19 virus AKA Coronavirus compiled
-          by Johns Hopkins University. Accepts: country as filter parameter,
-          otherwise returns all stats. Country name must match exactly with what
-          is in the data (URL encoded spaces and punctuation) For some reason
-          RapidAPI counts 304 "Not Modified" responses as "errors". This is the
-          reason for the high "error count" of this API.
-          <a href="https://rapidapi.com/KishCom/api/covid-19-coronavirus-statistics/details">
-            More information on this API can be found here.
-          </a>
-        </p>
       </div>
     </div>
   );
@@ -68,16 +70,31 @@ function App() {
 export default App;
 
 const styles = {
-  row: {
-    display: "flex",
-    margin: "50px"
+  statsContainer: {
+    height: "100%",
+    width: "100%",
+    display: "inline-block",
+    position: "relative",
+    paddingTop: "50px",
+    maxHeight: "750px"
   },
-  column: {
-    flex: "50%"
+  graphColumn: {
+    width: "50%",
+    height: "500px",
+    display: "inline-block",
+    position: "absolute",
+    left: "14px",
+    top: "50px"
+  },
+  apiInfoColumn: {
+    width: "450px",
+    height: "300px",
+    display: "inline-block",
+    position: "absolute",
   },
   banner: {
     width: "100%",
-    height: "190px",
+    height: "145px",
     backgroundColor: "#3a82c1",
     display: "relative",
   },
@@ -87,21 +104,26 @@ const styles = {
   },
   imageContainer: {
     position: "absolute",
-    float: "left",
-    left: "30px",
-    top: "-40px",
+    left: "20%",
+    top: "-50px",
     width: "400px",
-    height: "230px",
+    height: "195px",
     overflow: "hidden",
 
   },
-  textContainer: {
+  bannerTextContainer: {
     zPostion: "5",
-    position: "absolute",
-    width: "50%",
+    position: "relative",
+    width: "100%",
     color: "white",
     display: "inline-block",
-    left: "25%",
-    right: "25%"
+  },
+  header:{
+    display: "inline-block",
+    width: "100%"
+  },
+  body: {
+    width: "100%",
+    height: "100%"
   }
 };
--- src/BarGraph.jsx
+++ src/BarGraph.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom"
-import "./App.css"
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
 import {
   BarChart,
   Bar,
@@ -12,31 +12,67 @@ import {
   Legend,
   ResponsiveContainer
 } from "recharts";
-const Example = ({ covid19Stats }) => {
-  const formatData = (stats) => {
-    const newArray = [];
-    stats.forEach((dp) => {
-      // see if country is in array
-      const index = newArray.findIndex((i) => i.name == dp.country);
-      if (index == -1) {
-        newArray.push({
-          name: dp.country,
-          deaths: dp.deaths,
-          confirmed: dp.confirmed,
-          recovered: dp.recovered,
-        });
-      } else {
-        newArray[index].deaths += dp.deaths;
-        newArray[index].confirmed += dp.confirmed;
-        newArray[index].recovered += dp.recovered;
-      }
-    });
-    newArray.sort((a,b) => b.confirmed - a.confirmed)
-    return newArray.slice(0,10);
-  };
-  const data = formatData(covid19Stats);
-  return (
-    <div style={styles.barGraph}>
+
+function getIntroOfPage(label) {
+  if (label === "Page A") {
+    return "Page A is about men's clothing";
+  }
+  if (label === "Page B") {
+    return "Page B is about women's dress";
+  }
+  if (label === "Page C") {
+    return "Page C is about women's bag";
+  }
+  if (label === "Page D") {
+    return "Page D is about household goods";
+  }
+  if (label === "Page E") {
+    return "Page E is about food";
+  }
+  if (label === "Page F") {
+    return "Page F is about baby food";
+  }
+}
+
+function CustomTooltip({ payload, label, active }) {
+  if (active) {
+    return (
+      <div className="custom-tooltip">
+        <p className="label">{`${label} : ${payload[0].value}`}</p>
+        <p className="intro">{getIntroOfPage(label)}</p>
+        <p className="desc">Anything you want can be displayed here.</p>
+      </div>
+    );
+  }
+
+  return null;
+}
+const formatData = stats => {
+  const newArray = [];
+  stats.forEach(dp => {
+    // see if country is in array
+    const index = newArray.findIndex(i => i.name == dp.country);
+    if (index == -1) {
+      newArray.push({
+        name: dp.country,
+        deaths: dp.deaths,
+        confirmed: dp.confirmed,
+        recovered: dp.recovered
+      });
+    } else {
+      newArray[index].deaths += dp.deaths;
+      newArray[index].confirmed += dp.confirmed;
+      newArray[index].recovered += dp.recovered;
+    }
+  });
+  newArray.sort((a, b) => b.confirmed - a.confirmed);
+  return newArray.slice(0, 10);
+};
+export default class Graph extends Component {
+
+  render() {
+    const data = formatData(this.props.covid19Stats);
+    return (
       <ResponsiveContainer>
         <BarChart
           data={data}
@@ -47,25 +83,16 @@ const Example = ({ covid19Stats }) => {
             bottom: 5
           }}
         >
-          <CartesianGrid strokeDasharray="3 3" />
+          {/* <CartesianGrid strokeDasharray="3 3" /> */}
           <XAxis dataKey="name" />
           <YAxis />
-          <Tooltip />
+          <Tooltip content={<CustomTooltip />} />
           <Legend />
           <Bar dataKey="deaths" fill="red" />
           <Bar dataKey="confirmed" fill="blue" />
           <Bar dataKey="recovered" fill="#42CA9D" />
         </BarChart>
       </ResponsiveContainer>
-    </div>
-  );
-};
-
-export default Example
-
-const styles = {
-  barGraph: {
-    height: "100%",
-    maxHeight: "600px"
+    );
   }
 }
